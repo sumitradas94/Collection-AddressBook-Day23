85c7b60c7c06424c21cc3bdabc35184bdf1655f8
sumitradas94 <sumitradas94@gmail.com>
2023-01-26 14:22:01 +0530

    AbilitytoCreateContact

---
+++ AddressBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionAddressBook
+{
+    public class AddressBook
+    {
+        public static List<Person> People = new List<Person>();
+        public void createUser()
+        {
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter City : ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZipCode: ");
+            person.ZipCode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNum = Console.ReadLine();
+
+            Console.Write("Enter EmailId: ");
+            person.EmailId = Console.ReadLine();
+
+            People.Add(person);
+        }
+        public void printUser()
+        {
+            foreach (var person in People)
+            {
+                Console.WriteLine("**** Peoples present in list are : ");
+                Console.WriteLine(" FirstName: {0},\n LastName: {1},\n Adress: {2},\n City :\n {3},\n State: {4},\n Zip: {5},\n PhoneNum: {6},\n Email: {7}", person.FirstName, person.LastName, person.Address, person.City, person.State, person.ZipCode, person.PhoneNum, person.EmailId);
+            }
+        }
+
+    }
+}
+
+    
