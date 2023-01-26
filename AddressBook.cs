695168d922ffff9a4ede65aae750cd85007d1810
sumitradas94 <sumitradas94@gmail.com>
2023-01-26 14:39:59 +0530

    AbilitytoDeleteaPerson

--- AddressBook.cs
+++ AddressBook.cs
@@ -54,6 +54,86 @@ namespace CollectionAddressBook
                 Console.WriteLine(" FirstName: {0},\n LastName: {1},\n Adress: {2},\n City : {3},\n State: {4},\n Zip: {5},\n PhoneNum: {6},\n Email: {7}", person.FirstName, person.LastName, person.Address, person.City, person.State, person.ZipCode, person.PhoneNum, person.EmailId);
             }
         }
+        public void editContact()
+        {
+            if (People.Count != 0)
+            {
+                Console.WriteLine("Enter the First Name of contact For editing details:");
+                string Modified = Console.ReadLine();
+                foreach (var person in People)
+                {
+                    if (person.FirstName.ToUpper() == Modified.ToUpper())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Which information do you want to Edit?");
+                            Console.WriteLine("#1: Firstname, #2: Lastname, 3#: Address, 4#: City, 5#: State, 6#: Zipcode, 7#: PhoneNum, 8#: EmailId, 9#: Exit");
+                            int userOption = Convert.ToInt32(Console.ReadLine());
+                            switch (userOption)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    person.FirstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    person.LastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    person.Address = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter the New City: ");
+                                    person.City = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter the New State: ");
+                                    person.State = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    person.ZipCode = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.PhoneNum = Console.ReadLine();
+                                    break;
+                                case 8:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.PhoneNum = Console.ReadLine();
+                                    break;
+                                case 9:
+                                    return;
 
+                            }
+
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter the valid name!");
+            }
+        }
+
+        public void deleteContact()
+        {
+            Console.WriteLine("Enter the first name of the person you would like to remove.");
+            string Remove = Console.ReadLine();
+            foreach (var person in People.ToList())
+            {
+                if (person.FirstName.ToUpper() == Remove.ToUpper())
+                {
+                    People.Remove(person);
+                    Console.WriteLine("Contact is deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Contact is not present");
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,50 @@
         {
 
 
+            bool choice = true;
+
             AddressBook Adr = new AddressBook();
             Adr.createUser();
-            Adr.editContact();
-            Adr.printUser();
+
+            static void UserChoice()
+            {
+                Console.WriteLine("Choose one of the following options: ");
+                Console.WriteLine("#1 Create new user");
+                Console.WriteLine("#2 Edit user information");
+                Console.WriteLine("#3 Delete existing user");
+                Console.WriteLine("#4 Show all users in adressBook");
+                Console.WriteLine("#5 Exit");
+            }
+            while (choice)
+            {
+                UserChoice();
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        Adr.createUser();
+                        Adr.printUser();
+                        break;
+                    case 2:
+                        Adr.editContact();
+                        Adr.printUser();
+                        break;
+                    case 3:
+                        Adr.deleteContact();
+                        Adr.printUser();
+                        break;
+                    case 4:
+                        Adr.printUser();
+                        break;
+                    case 5:
+                        choice = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid option");
+                        break;
+
+                }
+            }
         }
     }
 }
